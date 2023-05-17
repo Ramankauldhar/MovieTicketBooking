1e3ec707e56601358f2561a9758a3029eea2b601
Ramankauldhar <rmnkaul979697@gmail.com>
2023-05-17 19:24:23 -0400

    V4: Login component Integration done with backend

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import {AuthService} from 'src/app/service/auth.service';
 
 @Component({
@@ -9,7 +10,12 @@ import {AuthService} from 'src/app/service/auth.service';
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup
-  constructor(private formBuilder: FormBuilder, private auth:AuthService){
+  message: String = "";
+  isProcess: boolean = false;
+  className = 'd-none'; //display none for alert in Html
+
+  
+  constructor(private formBuilder: FormBuilder, private auth:AuthService, private router:Router){
     this.loginForm = this.formBuilder.group({
       'email':['', Validators.required],
       'pswd':['', Validators.required],
@@ -19,11 +25,20 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void { }
   
   login(){
+    this.isProcess = true;
     const data = this.loginForm.value;
     this.auth.login(data).subscribe((res)=>{
-  
+      if(res.success){
+        localStorage.setItem('token', res.token)
+        this.router.navigate(['/dashboard'])
+      }
+      else{
+        this.isProcess = false;
+        this.message = res.message;
+        this.className = 'alert alert-danger';
+      }
     }, error=>{
-      
+      alert("Login Failed")
     })
   }
   }
